b236b07750d1224f7c7ba319c5a0a647be49a61e
IT INTERN <IT-INTERN@MRSHOLDINGS.LOCAL>
2024-03-18 07:35:03 -0700

    initial commit

---
+++ src/components/styles/js/global.jsx
@@ -0,0 +1,10 @@
+// import "./functions.js";
+import "./jquery.js";
+// import "./lightcase.js";
+// import "./bootstrap.bundle.min.js";
+// import "./circularProgressBar.min.js";
+// import "./fontawesome.min.js";
+// import "./isotope.pkgd.min.js";
+// import "./swiper.min.js";
+// import "./waypoints.min.js";
+// import ""
